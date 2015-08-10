2d196ea48ebea3a2f73443797b8e3ede6aa6777f
Dave Lively <davelively14@gmail.com>
2015-08-09 23:36:15 -0400

    Implemented show product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @store = Store.find(@product.store_id)
   end
 
   # GET /products/new
@@ -29,7 +30,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to store_path(@product.store_id), notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
