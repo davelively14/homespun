a35f58ab7134ac4a824fe026fe0c1bdb256bdaec
Dave Lively <davelively14@gmail.com>
2015-08-12 22:18:36 -0400

    Added Cart model, no logic yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811010148) do
+ActiveRecord::Schema.define(version: 20150812020154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20150811010148) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "store_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
