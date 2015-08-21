f71040a431eadde8fd8680634283e63a691baffb
Dave Lively <davelively14@gmail.com>
2015-08-20 22:04:27 -0400

    Added basic cart functionality.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :store
   has_many :line_items
   has_many :orders, :through => :line_items
+
+  before_destroy :ensure_no_line_items
+
+  def ensure_no_line_items
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812020154) do
+ActiveRecord::Schema.define(version: 20150821020124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150812020154) do
     t.integer  "quantity"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "cart_id"
   end
 
   create_table "orders", force: :cascade do |t|
