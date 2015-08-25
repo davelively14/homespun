8189bae1fe4ffd3573a5657feadc02c56220b788
Dave Lively <davelively14@gmail.com>
2015-08-25 13:52:17 -0400

    Removed cart-user association

--- db/migrate/20150821214334_add_user_id_column_to_carts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdColumnToCarts < ActiveRecord::Migration
-  def change
-    add_column :carts, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821214334) do
+ActiveRecord::Schema.define(version: 20150821021132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20150821214334) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
