3292a7f32c298ffff5052593fdb65f1d66be6be4
Dave Lively <davelively14@gmail.com>
2015-08-30 22:09:00 -0400

    Added addresses to edit_profile

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -69,6 +69,6 @@ class AddressesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def address_params
-      params.require(:address).permit(:user_id, :street1, :street2, :city, :city, :state, :zip)
+      params.require(:address).permit(:user_id, :street1, :street2, :city, :city, :state, :zip, :nick)
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def edit
+    @addresses = Address.where(user_id: current_user.id)
+  end
+
   protected
 
   def after_update_path_for(resource)
