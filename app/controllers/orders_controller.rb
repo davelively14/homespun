06d062093621dc70b774febf85711cb2092b5039
Dave Lively <davelively14@gmail.com>
2015-09-06 12:58:22 -0400

    Render sign-in, creating checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,11 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    if current_user
+      @user = User.find(current_user)
+    elsif session[:guest_id]
+      @guest = Guest.find(session[:guest_id])
+    end
   end
 
   # GET /orders/1/edit
